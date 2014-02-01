ba9f43e85497e8fd4ba5429e65706ab730111cc9
Andrew Stephens <boss@andrewanimation.biz>
2014-02-01 17:05:52 -0500

    Initial Commit

---
+++ inject.js
@@ -0,0 +1,8 @@
+//alert('Hello');
+
+/*window.setTimeout(function() {
+	// Change Title
+	$('#TFP_GUEST_WELCOME_HMPG_Data').find('h2').html('Welcome to gSIS!');
+}, 5000);*/
+
+$('#ptpgltbody_TFP_GUEST_WELCOME_HMPG').html('<h2>Welcome to gSIS!</h2>');
