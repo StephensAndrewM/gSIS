e05f3cebc238d69e427867786b68d4920680e194
Andrew Stephens <boss@andrewanimation.biz>
2014-02-02 08:11:45 -0500

    Login Page Has a Cat

--- login.js
+++ login.js
@@ -1,1 +1,12 @@
-//$('body *').hide();
+(function() {
+
+	if (window.location.href.indexOf("PAPP_GUEST") === -1) { return; }
+
+	$('html').css({ height:'100%' });
+	$('body').addClass('login-page');
+
+	$('body').append('<div id="gsis_login_form"></div>');
+	$('body').append('<div id="gsis_login_logo">gSIS</div>');
+	$('#gsis_login_form').html($('#ptpgltbody_row_TFP_GUEST_SIGNIN_PG').html());
+
+})();
