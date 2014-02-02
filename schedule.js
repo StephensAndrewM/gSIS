72229a12bb4855da452e2ce07518caf22ab7152e
Colin Watts <colin.watts@tufts.edu>
2014-02-02 11:06:39 -0500

    I dunno

--- schedule.js
+++ schedule.js
@@ -1,9 +1,8 @@
+pageAction(['https://isis.uit.tufts.edu/psp/paprd/EMPLOYEE/PSFT_SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL'], function() {
 
 
-var path = window.location.pathname;
+window.location.href = "javascript:submitAction_win0(document.win0,'DERIVED_SSS_ENR_SS_CLS_SCHED_LINK');";
 
-if (path == 'https://isis.uit.tufts.edu/psp/paprd/EMPLOYEE/PSFT_SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL#schedule')
-{
 
-	window.location.href = "javascript:submitAction_win0(document.win0,'DERIVED_SSS_ENR_SS_CLS_SCHED_LINK');"
-}
+
+});
