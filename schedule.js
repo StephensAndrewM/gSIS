ad6e4c125f8dad71704b65426d82df8cbdb9790a
Colin Watts <colin.watts@tufts.edu>
2014-02-02 09:28:08 -0500

    Holy shit, how do we find the schedule?

---
+++ schedule.js
@@ -0,0 +1,9 @@
+
+
+var path = window.location.pathname;
+
+if (path == 'https://isis.uit.tufts.edu/psp/paprd/EMPLOYEE/PSFT_SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL#schedule')
+{
+
+	window.location.href = "javascript:submitAction_win0(document.win0,'DERIVED_SSS_ENR_SS_CLS_SCHED_LINK');"
+}
